72dabf243306f4965fada3a420367b92e93e2bfc
Prajjwal <prajjwal.pandey@tothenew.com>
2023-02-11 12:51:09 +0530

    new test

--- src/main/java/com/logger/controller/Controller.java
+++ src/main/java/com/logger/controller/Controller.java
@@ -18,7 +18,7 @@ public class Controller {
   public Employee logEmployeeDetails() throws JsonProcessingException {
     Employee employee = new Employee();
     employee.setId("1");
-    employee.setName("Prajjwal");
+    employee.setName("Prajjwal_Test");
     DepartMent departMent = new DepartMent();
     departMent.setDepartMentName("TTN");
     departMent.setDepartMentType("CSE");
